23f17145606a0a5489b921b527e0c02d9727f5ed
Sevket YILMAZ <sevketyilmaz@gmail.com>
2014-07-26 18:27:19 +0300

    L03: eov30 : Preference, metric to imperial changes on the fly.

--- app/src/main/java/tr/com/example/sunshine/app/ForecastFragment.java
+++ app/src/main/java/tr/com/example/sunshine/app/ForecastFragment.java
@@ -285,6 +285,24 @@ public class ForecastFragment extends Fragment {
 
         // Prepare the weather high/lows for presentation.
         private String formatHiAndLow(double hi, double low) {
+            // Data is fetched in Celsius by default.
+            // If user prefers to see in Fahrenheit, convert the values here.
+            // We do this rather than fetching in Fahrenheit so that the user can
+            // change this option without us having to re-fetch the data once
+            // we start storing the values in a database.
+            SharedPreferences sharedPrefs =
+                    PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = sharedPrefs.getString(
+                    getString(R.string.pref_units_key),
+                    getString(R.string.pref_units_metric));
+
+            if (unitType.equals(getString(R.string.pref_units_imperial))) {
+                hi = (hi * 1.8) + 32;
+                low = (low * 1.8) + 32;
+            } else if (!unitType.equals(getString(R.string.pref_units_metric))) {
+                Log.d(TAG, "Unit type not found: " + unitType);
+            }
+
             long roundedHigh = Math.round(hi);
             long roundedLow = Math.round(low);
 
