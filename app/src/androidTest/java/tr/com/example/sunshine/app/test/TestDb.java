b78f221abc67d03142a97a6c038e70fde9cc866c
Sevket YILMAZ <sevketyilmaz@gmail.com>
2014-08-16 18:51:10 +0300

    L04: eov27 : Weather table entry tests done in TestDb.java

--- app/src/androidTest/java/tr/com/example/sunshine/app/test/TestDb.java
+++ app/src/androidTest/java/tr/com/example/sunshine/app/test/TestDb.java
@@ -6,8 +6,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
-import tr.com.example.sunshine.app.data.WeatherContract.LocationEntry;
 import tr.com.example.sunshine.app.data.WeatherDbHelper;
+import tr.com.example.sunshine.app.data.WeatherContract.*;
 
 /**
  * Created by svkt on 16.08.2014.
@@ -93,6 +93,60 @@ public class TestDb extends AndroidTestCase{
             fail("no values returned :(/");
         }
 
+        /*Now do the things for weather table*/
+        ContentValues weatherValues = new ContentValues();
+        weatherValues.put(WeatherEntry.COLUMN_LOC_KEY, locationRowId);
+        weatherValues.put(WeatherEntry.COLUMN_DATETEXT, "20141205");
+        weatherValues.put(WeatherEntry.COLUMN_DEGREES, 1.1);
+        weatherValues.put(WeatherEntry.COLUMN_HUMIDITY, 1.2);
+        weatherValues.put(WeatherEntry.COLUMN_PRESSURE, 1.3);
+        weatherValues.put(WeatherEntry.COLUMN_MAX_TEMP, 75);
+        weatherValues.put(WeatherEntry.COLUMN_MIN_TEMP, 65);
+        weatherValues.put(WeatherEntry.COLUMN_SHORT_DESC, "Asteroids");
+        weatherValues.put(WeatherEntry.COLUMN_WIND_SPEED, 5.5);
+        weatherValues.put(WeatherEntry.COLUMN_WEATHER_ID, 321);
+
+        long weatherRowId = db.insert(WeatherEntry.TABLE_NAME, null, weatherValues);
+        assertTrue(weatherRowId != -1);
+
+        // A cursor is your primary interface to the query results.
+        Cursor weatherCursor = db.query(
+                WeatherEntry.TABLE_NAME,  // Table to Query
+                null, // leaving "columns" null just returns all the columns.
+                null, // cols for "where" clause
+                null, // values for "where" clause
+                null, // columns to group by
+                null, // columns to filter by row groups
+                null  // sort order
+        );
+
+        if (!weatherCursor.moveToFirst()) {
+            fail("No weather data returned!");
+        }
+
+        assertEquals(weatherCursor.getInt(
+                weatherCursor.getColumnIndex(WeatherEntry.COLUMN_LOC_KEY)), locationRowId);
+        assertEquals(weatherCursor.getString(
+                weatherCursor.getColumnIndex(WeatherEntry.COLUMN_DATETEXT)), "20141205");
+        assertEquals(weatherCursor.getDouble(
+                weatherCursor.getColumnIndex(WeatherEntry.COLUMN_DEGREES)), 1.1);
+        assertEquals(weatherCursor.getDouble(
+                weatherCursor.getColumnIndex(WeatherEntry.COLUMN_HUMIDITY)), 1.2);
+        assertEquals(weatherCursor.getDouble(
+                weatherCursor.getColumnIndex(WeatherEntry.COLUMN_PRESSURE)), 1.3);
+        assertEquals(weatherCursor.getInt(
+                weatherCursor.getColumnIndex(WeatherEntry.COLUMN_MAX_TEMP)), 75);
+        assertEquals(weatherCursor.getInt(
+                weatherCursor.getColumnIndex(WeatherEntry.COLUMN_MIN_TEMP)), 65);
+        assertEquals(weatherCursor.getString(
+                weatherCursor.getColumnIndex(WeatherEntry.COLUMN_SHORT_DESC)), "Asteroids");
+        assertEquals(weatherCursor.getDouble(
+                weatherCursor.getColumnIndex(WeatherEntry.COLUMN_WIND_SPEED)), 5.5);
+        assertEquals(weatherCursor.getInt(
+                weatherCursor.getColumnIndex(WeatherEntry.COLUMN_WEATHER_ID)), 321);
+
+        weatherCursor.close();
+        dbHelper.close();
     }
 
 }
