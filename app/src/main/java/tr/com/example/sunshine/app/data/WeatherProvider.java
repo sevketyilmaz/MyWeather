92ddafe32cfa994af84fa2ff4f74984935cbb68a
Sevket YILMAZ <sevketyilmaz@gmail.com>
2014-08-18 09:17:54 +0300

    L04B: eov08 : WeatherProvider.java added

---
+++ app/src/main/java/tr/com/example/sunshine/app/data/WeatherProvider.java
@@ -0,0 +1,47 @@
+package tr.com.example.sunshine.app.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * Created by svkt on 18.08.2014.
+ */
+public class WeatherProvider extends ContentProvider {
+    private static final int WEATHER = 100;
+    private static final int WEATHER_WITH_LOCATION = 101;
+    private static final int WEATHER_WITH_LOCATION_AND_DATE = 102;
+    private static final int LOCATION = 300;
+    private static final int LOCATION_ID = 301;
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings2, String s2) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
